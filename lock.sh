0a6ddecdd8f7675dc8862fe687237d657ebaac97
Cyan Cipher <cyan@pop-os.localdomain>
2023-06-30 14:15:26 +0530

    Lock screen modified

--- lock.sh
+++ lock.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
 
-i3lock -i ~/.config/awesome/lock.jpg | systemctl suspend
+UNAME=$(awk -F':' -v uid=1000 '$3 == uid { print $1 }' /etc/passwd)
+i3lock -e -k -i ~/.config/awesome/lock.jpg --bar-indicator --greeter-text "Hey, $UNAME!" --greeter-font Hack Nerd Font --greeter-pos "680:320" --bar-color 290200E2 --bar-max-height 20 --greeter-color FFFFFF --greeter-size 70 --time-size 50 --date-size 20 --time-color FFFFFF --date-color FFFFFF | systemctl suspend
